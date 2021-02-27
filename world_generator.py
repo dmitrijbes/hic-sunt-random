5471ffefdefdda2cc436aded623e07f2ab11afbd
Dima Bes <dmitrij.beskrestnov@gmail.com>
2021-02-27 22:45:09 +0200

    Simple stub rendering

--- world_generator.py
+++ world_generator.py
@@ -1,4 +1,4 @@
-import world_entities
+from world_entities import World
 
 def generate_world():
-    return World("Great Beet", 20, 20)
+    return World("Great Beet", 5, 5)
--- world_renderer.py
+++ world_renderer.py
@@ -1,2 +1,6 @@
 def render_world(world):
-    print("Rendering world:")
+    print("Rendering world:", world.name)
+    for x in range(world.size[0]):
+        for y in range(world.size[1]):
+            print("X", end="")
+        print("\n", end="")
