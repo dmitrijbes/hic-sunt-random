454a660c2d14c8e66ef1c7c643a1058703dcbe06
Леля Клер <sedrickedel@gmail.com>
2021-03-14 22:38:11 +0200

    world_entities has been changet, classes added!

--- world_entities.py
+++ world_entities.py
@@ -5,3 +5,26 @@ class World:
 
     def __str__(self):
         return f"World; name:{self.name} size_x:{self.size[0]} size_y:{self.size[1]}"
+
+_new_world_matrix = [
+    []
+]
+
+object = 
+
+
+class WorldObject:
+    def __init__(self, coordinate_x, coordinate_y)
+        self.position = (coordinate_x, coordinate_y)
+
+
+class Field(WorldObject):
+    pass
+
+
+class Ocean(WorldObject):
+    pass
+
+
+class Mountain(WorldObject):
+    pass
