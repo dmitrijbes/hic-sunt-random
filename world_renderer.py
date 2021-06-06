448f382b1228c04ce5901548389820a0eab5f347
Dima Bes <dmitrij.beskrestnov@gmail.com>
2021-06-06 21:02:00 +0300

    Improve world renderer, fix single line

--- world_renderer.py
+++ world_renderer.py
@@ -3,43 +3,30 @@ import cv2
 import world_entities
 import copy
 
+def get_world_object_img(world_object):
+    if isinstance(world_object, world_entities.Mountain):
+        return cv2.imread('./resources/tiles/mountain.png', cv2.IMREAD_COLOR)
+    elif isinstance(world_object, world_entities.Field):
+        return cv2.imread('./resources/tiles/field.png', cv2.IMREAD_COLOR)
+    elif isinstance(world_object, world_entities.Ocean):
+        return cv2.imread('./resources/tiles/ocean.png', cv2.IMREAD_COLOR)
+    else:
+        return cv2.imread('./resources/tiles/placeholder.png', cv2.IMREAD_COLOR)
+
 def render_world(world):
     print("Rendering world:", world.name)
 
-    world_picture = cv2.imread('./resources/tiles/world_begins.png', cv2.IMREAD_COLOR)
-    
-    img1 = cv2.imread('./resources/tiles/ocean.png', cv2.IMREAD_COLOR)
-    img2 = cv2.imread('./resources/tiles/mountain.png', cv2.IMREAD_COLOR)
-    img3 = cv2.imread('./resources/tiles/field.png', cv2.IMREAD_COLOR)
-    img4 = cv2.imread('./resources/tiles/space.png', cv2.IMREAD_COLOR)
-    
-    world_map = []
-    for world_objects_row in world.world_objects:
+    world_picture_temp = []
+    for row_index, world_objects_row in enumerate(world.world_objects):
+        world_picture_row = get_world_object_img(world.world_objects[row_index][0])
+
         for world_object in world_objects_row:
-            if isinstance(world_object, world_entities.Mountain):
-                world_picture = np.concatenate((world_picture, img2), axis=1)
-            elif isinstance(world_object, world_entities.Field):
-                world_picture = np.concatenate((world_picture, img3), axis=1)
-            elif isinstance(world_object, world_entities.Ocean):
-                world_picture = np.concatenate((world_picture, img1), axis=1)
-            else:
-                world_picture = np.concatenate((world_picture, img4), axis=1)
+            world_picture_row = np.concatenate((world_picture_row, get_world_object_img(world_object)), axis=1)
 
-        world_picture_row = copy.deepcopy(world_picture)
-        world_map.append(world_picture_row)
-        world_picture = cv2.imread('./resources/tiles/world_begins.png', cv2.IMREAD_COLOR)
-    
-    final_world_map = world_map[0]
-    
-    for picture_row in world_map[1:]:
-        final_world_map = np.concatenate((final_world_map, picture_row), axis=0)
+        world_picture_temp.append(copy.deepcopy(world_picture_row))
 
-    cv2.imwrite('./output/out.png', final_world_map)
-    # cv2.imshow('./output/out.png', final_world_map)
+    world_picture = world_picture_temp[0]
+    for world_picture_row in world_picture_temp[1:]:
+        world_picture = np.concatenate((world_picture, world_picture_row), axis=0)
 
-    # k = cv2.waitKey()
-    # if k == 27:         # wait for ESC key to exit
-    #     cv2.destroyAllWindows()
-    # elif k == ord('s'):  # wait for 's' key to save and exit
-    #     cv2.imwrite('out.png', final_world_map)
-    #     cv2.destroyAllWindows()
+    cv2.imwrite('./output/out.png', world_picture)
