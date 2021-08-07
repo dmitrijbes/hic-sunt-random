76d3e76035b9cdf2c712b236bf163f76db93b3f3
Dima Bes <dmitrij.beskrestnov@gmail.com>
2021-08-07 19:55:04 +0300

    Separate growth methods into separate file.

--- world_generator.py
+++ world_generator.py
@@ -1,4 +1,5 @@
 import world_entities
+import world_growth
 import random
 import copy
 
@@ -6,33 +7,6 @@ def generate_ocean(world):
     for x, world_objects_row in enumerate(world.world_objects):
         for y, world_object in enumerate(world_objects_row):
             world.world_objects[x][y] = world_entities.Ocean(x, y)
-
-def is_inside_world(world, x, y):
-    if x < 0 or y < 0:
-        return False
-
-    world_size_x, world_size_y = world.size
-    if (x > world_size_x - 1) or (y > world_size_y - 1):
-        return False
-
-    return True
-
-def get_neighbours_count(world, world_object, x, y):
-    neighbours_count = 0
-
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (-1, 1), (1, 1), (1, -1)]
-    for direction_x, direction_y in directions:
-        neighbour_x = x + direction_x
-        neighbour_y = y + direction_y
-        if is_inside_world(world, neighbour_x, neighbour_y) and isinstance(world.world_objects[neighbour_x][neighbour_y], world_object):
-            neighbours_count += 1
-
-    return neighbours_count
-
-def throw_coin():
-    if random.randint(0, 1) == 1:
-        return True
-    return False
 
 def generate_land(world, land_seeds, land_grow_rate):
     for i in range(land_seeds):
@@ -63,56 +37,15 @@ def generate_land(world, land_seeds, land_grow_rate):
                     for direction_x, direction_y in characters[rand_character]:
                         land_grow_x = x + direction_x
                         land_grow_y = y + direction_y
-                        if is_inside_world(world, land_grow_x, land_grow_y) and throw_coin():
+                        if world_growth.is_inside_world(world, land_grow_x, land_grow_y) and world_growth.throw_coin():
                                 world.world_objects[land_grow_x][land_grow_y] = world_entities.Field(land_grow_x, land_grow_y)
 
-def get_mountain_grow_directions(directions, world, x, y):
-    grow_directions = []
-    for direction_x, direction_y in directions:
-        mountain_grow_x = x + direction_x
-        mountain_grow_y = y + direction_y
-        if is_inside_world(world, mountain_grow_x, mountain_grow_y) and isinstance(world.world_objects[mountain_grow_x][mountain_grow_y], world_entities.Field):
-            grow_directions.append((direction_x, direction_y))
+def generate_mountains(world, mountain_seeds_amount, mountain_grow_rate):
+    world_growth.plant_seeds(world, world_entities.Mountain, mountain_seeds_amount, world_entities.Field)
 
-    return grow_directions
+    characters = world_growth.create_characters(3, 2)
 
-def generate_mountains(world, mountain_seeds, mountain_grow_rate, mountain_grow_size):
-    for i in range(mountain_seeds):
-        world_size_x, world_size_y = world.size
-        random_pos_x = random.randint(1, world_size_x - 2)
-        random_pos_y = random.randint(1, world_size_y - 2)
-        while not isinstance(world.world_objects[random_pos_x][random_pos_y], world_entities.Field):
-            random_pos_x = random.randint(1, world_size_x - 2)
-            random_pos_y = random.randint(1, world_size_y - 2)
-        world.world_objects[random_pos_x][random_pos_y] = world_entities.Mountain(random_pos_x, random_pos_y)
-
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (-1, 1), (1, 1), (1, -1)]
-    characters = []
-    max_characters = 3
-    character_force = 2
-    for j in range(max_characters):
-        character_directions = []
-        for i in range(character_force):
-            random_direction = random.randint(0, len(directions) - 1)
-            character_directions.append(directions[random_direction])
-        characters.append(character_directions)
-
-    for i in range(mountain_grow_rate):
-        temp_world_objects = copy.deepcopy(world.world_objects)
-        for x, world_objects_row in enumerate(temp_world_objects):
-            for y, world_object in enumerate(world_objects_row):
-                if isinstance(world_object, world_entities.Mountain):
-                    rand_character = random.randint(0, len(characters) - 1)
-                    grow_directions = get_mountain_grow_directions(characters[rand_character], world, x, y)
-
-                    if not grow_directions:
-                        continue
-
-                    direction_index = random.randint(0, len(grow_directions) - 1)
-                    direction_x, direction_y = grow_directions[direction_index]
-                    mountain_grow_x = x + direction_x
-                    mountain_grow_y = y + direction_y
-                    world.world_objects[mountain_grow_x][mountain_grow_y] = world_entities.Mountain(mountain_grow_x, mountain_grow_y)
+    world_growth.grow_seeds(world, characters, world_entities.Mountain, world_entities.Field, mountain_grow_rate)
 
 # # def 555get_mountain_grow_directions(directions, world, x, y):
 #     grow_directions = []
@@ -172,10 +105,10 @@ def get_river_grow_directions(river_grow_size, world, x, y):
         river_grow_x = x + direction_x
         river_grow_y = y + direction_y
         if not river_grow_directions:
-            if is_inside_world(world, river_grow_x, river_grow_y) and (isinstance(world.world_objects[river_grow_x][river_grow_y], world_entities.Field) or isinstance(world.world_objects[river_grow_x][river_grow_y], world_entities.Mountain)) and get_neighbours_count(world, world_entities.River, river_grow_x, river_grow_y) < river_grow_size:
+            if world_growth.is_inside_world(world, river_grow_x, river_grow_y) and (isinstance(world.world_objects[river_grow_x][river_grow_y], world_entities.Field) or isinstance(world.world_objects[river_grow_x][river_grow_y], world_entities.Mountain)) and world_growth.get_neighbours_count(world, world_entities.River, river_grow_x, river_grow_y) < river_grow_size:
                river_grow_directions.append((direction_x, direction_y))
         else:
-            if is_inside_world(world, river_grow_x, river_grow_y) and (isinstance(world.world_objects[river_grow_x][river_grow_y], world_entities.Field) or isinstance(world.world_objects[river_grow_x][river_grow_y], world_entities.Mountain)) and get_neighbours_count(world, world_entities.River, river_grow_x, river_grow_y) < river_grow_size and (direction_x, direction_y) != river_grow_directions[-1]:
+            if world_growth.is_inside_world(world, river_grow_x, river_grow_y) and (isinstance(world.world_objects[river_grow_x][river_grow_y], world_entities.Field) or isinstance(world.world_objects[river_grow_x][river_grow_y], world_entities.Mountain)) and world_growth.get_neighbours_count(world, world_entities.River, river_grow_x, river_grow_y) < river_grow_size and (direction_x, direction_y) != river_grow_directions[-1]:
                 river_grow_directions.append((direction_x, direction_y))
 
     return river_grow_directions
@@ -221,16 +154,13 @@ def generate_world(name, size_x, size_y):
     generate_land(world, land_seeds, land_grow_rate)
 
     mountain_seeds = 3
-    mountain_grow_rate = 6
-    mountain_grow_size = 3
-    generate_mountains(world, mountain_seeds, mountain_grow_rate, mountain_grow_size)
+    mountain_grow_rate = 10
+    generate_mountains(world, mountain_seeds, mountain_grow_rate)
 
     river_seeds = 5
     river_grow_rate = 10
     river_grow_size = 2
     generate_river(world, river_seeds, river_grow_rate, river_grow_size)
-
-
 
     generate_forest()
     generate_lake()
---
+++ world_growth.py
@@ -0,0 +1,93 @@
+import random
+import copy
+
+class Character():
+    def __init__(self, directions):
+        self.directions = directions
+
+    def get_directions(self):
+        return self.directions
+
+
+def create_characters(characters_amount, character_directions_amount, directions=[(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (1, 1), (-1, 1), (1, -1)]):
+    characters = []
+
+    for j in range(characters_amount):
+        character_directions = []
+        for i in range(character_directions_amount):
+            random_direction = random.randint(0, len(directions) - 1)
+            character_directions.append(directions[random_direction])
+        characters.append(Character(character_directions))
+
+    return characters
+
+
+def is_inside_world(world, x, y):
+    if x < 0 or y < 0:
+        return False
+
+    world_size_x, world_size_y = world.size
+    if (x > world_size_x - 1) or (y > world_size_y - 1):
+        return False
+
+    return True
+
+
+def get_neighbours_count(world, world_object, x, y):
+    neighbours_count = 0
+
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (-1, 1), (1, 1), (1, -1)]
+    for direction_x, direction_y in directions:
+        neighbour_x = x + direction_x
+        neighbour_y = y + direction_y
+        if is_inside_world(world, neighbour_x, neighbour_y) and isinstance(world.world_objects[neighbour_x][neighbour_y], world_object):
+            neighbours_count += 1
+
+    return neighbours_count
+
+
+def throw_coin():
+    if random.randint(0, 1) == 1:
+        return True
+    return False
+
+
+def get_grow_directions(world, directions, seed_x, seed_y, field_type):
+    grow_directions = []
+    for direction_x, direction_y in directions:
+        grow_x = seed_x + direction_x
+        grow_y = seed_y + direction_y
+        if is_inside_world(world, grow_x, grow_y) and isinstance(world.world_objects[grow_x][grow_y], field_type):
+            grow_directions.append((grow_x, grow_y))
+
+    return grow_directions
+
+
+def plant_seeds(world, seed_type, seeds_amount, field_type):
+    planted_seeds = []
+
+    for i in range(seeds_amount):
+        world_size_x, world_size_y = world.size
+        random_pos_x = random.randint(1, world_size_x - 2)
+        random_pos_y = random.randint(1, world_size_y - 2)
+        while not isinstance(world.world_objects[random_pos_x][random_pos_y], field_type) or (random_pos_x, random_pos_y) in planted_seeds:
+            random_pos_x = random.randint(1, world_size_x - 2)
+            random_pos_y = random.randint(1, world_size_y - 2)
+        world.world_objects[random_pos_x][random_pos_y] = seed_type(random_pos_x, random_pos_y)
+        planted_seeds.append((random_pos_x, random_pos_y))
+
+
+def grow_seeds(world, characters, seed_type, field_type, grow_rate):
+    for i in range(grow_rate):
+        temp_world_objects = copy.deepcopy(world.world_objects)
+        for x, world_objects_row in enumerate(temp_world_objects):
+            for y, world_object in enumerate(world_objects_row):
+                if isinstance(world_object, seed_type):
+                    rand_character = random.randint(0, len(characters) - 1)
+                    grow_directions = get_grow_directions(world, characters[rand_character].get_directions(), x, y, field_type)
+                    if not grow_directions:
+                        continue
+
+                    rand_direction_index = random.randint(0, len(grow_directions) - 1)
+                    grow_x, grow_y = grow_directions[rand_direction_index]
+                    world.world_objects[grow_x][grow_y] = seed_type(grow_x, grow_y)
