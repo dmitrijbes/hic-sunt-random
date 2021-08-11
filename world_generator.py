28149391d7d95e8453bde347c9ae22c0cacd7bec
Dima Bes <dmitrij.beskrestnov@gmail.com>
2021-08-11 20:43:42 +0300

    Refactor generate_land

--- world_generator.py
+++ world_generator.py
@@ -9,36 +9,11 @@ def generate_ocean(world):
             world.world_objects[x][y] = world_entities.Ocean(x, y)
 
 def generate_land(world, land_seeds, land_grow_rate):
-    for i in range(land_seeds):
-        world_size_x, world_size_y = world.size
-        random_pos_x = random.randint(1, world_size_x - 2)
-        random_pos_y = random.randint(1, world_size_y - 2)
+    world_growth.plant_seeds(world, world_entities.Field, land_seeds, world_entities.Ocean)
 
-        world.world_objects[random_pos_x][random_pos_y] = world_entities.Field(random_pos_x, random_pos_y)
+    characters = world_growth.create_characters(6, 2)
 
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (-1, 1), (1, 1), (1, -1)]
-    characters = []
-    max_characters = 6
-    character_force = 2
-    for j in range(max_characters):
-        character_directions = []
-        for i in range(character_force):
-            random_direction = random.randint(0, len(directions) - 1)
-            character_directions.append(directions[random_direction])
-        characters.append(character_directions)
-
-    for i in range(land_grow_rate):
-        temp_world_objects = copy.deepcopy(world.world_objects)
-        for x, world_objects_row in enumerate(temp_world_objects):
-            for y, world_object in enumerate(world_objects_row):
-                if isinstance(world_object, world_entities.Field):
-                    rand_character = random.randint(0, len(characters) - 1)
-
-                    for direction_x, direction_y in characters[rand_character]:
-                        land_grow_x = x + direction_x
-                        land_grow_y = y + direction_y
-                        if world_growth.is_inside_world(world, land_grow_x, land_grow_y) and world_growth.throw_coin():
-                                world.world_objects[land_grow_x][land_grow_y] = world_entities.Field(land_grow_x, land_grow_y)
+    world_growth.grow_seeds(world, characters, world_entities.Field, world_entities.Ocean, land_grow_rate)
 
 def generate_mountains(world, mountain_seeds_amount, mountain_grow_rate):
     world_growth.plant_seeds(world, world_entities.Mountain, mountain_seeds_amount, world_entities.Field)
