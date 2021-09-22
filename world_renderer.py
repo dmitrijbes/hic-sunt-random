a4113fe301c818f8f109bc265bca621ee9f3dea7
Dima Bes <dmitrij.beskrestnov@gmail.com>
2021-09-22 21:34:46 +0300

    Start multiple tiles implementation.

--- world_renderer.py
+++ world_renderer.py
@@ -1,36 +1,52 @@
 import copy
 import os.path
+import random
 
-import numpy as np
 import cv2
+import numpy as np
 
 import world_entities
 
 
-def get_world_object_img(world_object):
+TILES_PATH = './resources/tiles/'
+
+
+class TilesInfo:
+    def __init__(self):
+        self.tiles_info = {}
+
+    def init_info(self):
+
+
+def get_world_object_img(world_object, tiles_info):
+    tiles = tiles_info.get_tiles_info()
+
     if isinstance(world_object, world_entities.Mountain):
-        return cv2.imread('./resources/tiles/mountain.png', cv2.IMREAD_COLOR)
+        return cv2.imread(TILES_PATH + 'mountain.png', cv2.IMREAD_COLOR)
     elif isinstance(world_object, world_entities.Field):
-        return cv2.imread('./resources/tiles/field.png', cv2.IMREAD_COLOR)
+        return cv2.imread(TILES_PATH + 'field.png', cv2.IMREAD_COLOR)
     elif isinstance(world_object, world_entities.Ocean):
-        return cv2.imread('./resources/tiles/ocean.png', cv2.IMREAD_COLOR)
+        return cv2.imread(TILES_PATH + 'ocean.png', cv2.IMREAD_COLOR)
     elif isinstance(world_object, world_entities.River):
-        return cv2.imread('./resources/tiles/river.png', cv2.IMREAD_COLOR)
+        return cv2.imread(TILES_PATH + 'river.png', cv2.IMREAD_COLOR)
     else:
-        return cv2.imread('./resources/tiles/placeholder.png', cv2.IMREAD_COLOR)
+        return cv2.imread(TILES_PATH + 'placeholder.png', cv2.IMREAD_COLOR)
 
 
 def render_world(world):
     print("Rendering world:", world.name)
 
+    tiles_info = TilesInfo()
+    tiles_info.init_info()
+
     world_picture_temp = []
     for row_index, world_objects_row in enumerate(world.world_objects):
         world_picture_row = get_world_object_img(
-            world.world_objects[row_index][0])
+            world.world_objects[row_index][0], tiles_info)
 
         for world_object in world_objects_row[1:]:
             world_picture_row = np.concatenate(
-                (world_picture_row, get_world_object_img(world_object)), axis=1)
+                (world_picture_row, get_world_object_img(world_object, tiles_info)), axis=1)
 
         world_picture_temp.append(copy.deepcopy(world_picture_row))
 
